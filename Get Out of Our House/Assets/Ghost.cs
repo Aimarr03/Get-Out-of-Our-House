a9065a64806ad229cc08c85297b57c1df94a7abd
Raiduts <rivaldorakun1253@gmail.com>
2024-03-13 11:11:07 +0700

    Testing

---
+++ Get Out of Our House/Assets/Ghost.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghostmhjjhgc : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moving();
+    }
+
+    void moving()
+    {
+        float movingX = Input.GetAxis("Horizontal");
+        float movingY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(movingX * speed, movingY * speed);
+    }
+}
